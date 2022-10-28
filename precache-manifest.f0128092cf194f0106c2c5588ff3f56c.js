f24de8c8a444d691b336b0b7b21e0f7de9c832ba
Milton Ouma <ouma.nyang1@gmail.com>
2022-10-28 12:16:46 +0300

    Updates

---
+++ precache-manifest.f0128092cf194f0106c2c5588ff3f56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b1141f3c1dc4f78e73e8817a65e3c6",
+    "url": "/admin-dashboard/index.html"
+  },
+  {
+    "revision": "85ec989c70dc7967955a",
+    "url": "/admin-dashboard/static/css/main.67e7709d.chunk.css"
+  },
+  {
+    "revision": "185b8ba43ebef2e3d127",
+    "url": "/admin-dashboard/static/js/2.52d56548.chunk.js"
+  },
+  {
+    "revision": "85ec989c70dc7967955a",
+    "url": "/admin-dashboard/static/js/main.6da2784b.chunk.js"
+  },
+  {
+    "revision": "96c7ee2d7622ccfac4c4",
+    "url": "/admin-dashboard/static/js/runtime-main.1dac9b84.js"
+  },
+  {
+    "revision": "087f41c92d19cfc10bbe3d96aaed4250",
+    "url": "/admin-dashboard/static/media/5fg64gf67fhg.087f41c9.png"
+  }
+]);
